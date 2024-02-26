8bbc77e168e624348ceb51bf838bf3509b53e6ba
Zinam Mohamed <pk.x3311@gmail.com>
2024-02-27 02:47:43 +0300

    add .env dependency module that  loads environment variables from a .env file into process.env

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,12 +1,16 @@
 let {courses} = require('../data/courses')
 const {validationResult} = require('express-validator')
 const Course = require('../modules/courses.module');
+const httpStatusText = require('../utils/httpStatusText')
 
+/*
+find({queryFilter},{projection})
 
+*/
 const getAllCources = async (req,res)=>{
     // get all courses from mongodb using Course model
-    const courses = await Course.find();
-    res.json(courses);
+    const courses = await Course.find({},{"__v":false});
+    res.json({status: httpStatusText.SUCCESS ,data: {courses}});
 }
 
 const getSpecificCourse = async (req,res)=>{
@@ -14,36 +18,36 @@ const getSpecificCourse = async (req,res)=>{
         const courseId = req.params.id;
         const course = await Course.findById(courseId);
         if(!course){
-            res.status(404).json({msg:'Not Found'});
+            res.status(404).json({status: httpStatusText.FAIL ,Message: 'course not found'});
         }
-        return res.json(course);
+        return res.json({status: httpStatusText.SUCCESS ,data: {course}});
     }catch(error){
-        return res.status(400).json({msg:'Invalid object id',...error});
+        return res.status(400).json({status: httpStatusText.ERROR ,Message: error.Message});
     }
 }
 const addNewCourse = async (req,res)=>{
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json(errors.array());
+        return res.status(400).json({status: httpStatusText.FAIL,data: errors.array()});
     }
     try{
         const newCourse = new Course(req.body);
         await newCourse.save();
-        res.status(201).json(newCourse)
+        res.status(201).json({status: httpStatusText.SUCCESS,data: {course:newCourse}})
     }catch(error){
-        return res.status(400).json({msg:'added faield',...error});
+        return res.status(400).json({status: httpStatusText.ERROR, Message: error.Message});
     }
 }
 
 const updateCourse = async (req,res)=>{
     const courseId = req.params.id;
     try{
-        const updatedCourse = await Course.updateOne({_id:courseId},{$set:{...req.body}});
-        return res.status(200).json({msg:'updated successfully',...updatedCourse});
+        const updatedCourse = await Course.updateOne({_id: courseId}, {$set: {...req.body}});
+        return res.status(200).json({status: httpStatusText.SUCCESS, data: {course:updatedCourse}});
 
     }catch(error){
-        return res.status(400).json({msg:'Invalid object id',...error});
+        return res.status(400).json({status: httpStatusText.ERROR, Message: error.Message});
     }
 }
 
@@ -51,9 +55,9 @@ const deleteCourse = async (req,res)=>{
     const courseId = req.params.id;
     try{
         const course = await Course.deleteOne({_id:courseId});
-        res.status(200).json({msg:'deleted successfully',...course});;
+        res.status(200).json({status: httpStatusText.SUCCESS ,...course});;
     }catch(error){
-        return res.status(400).json({msg:'Invalid object id',...error});
+        return res.status(400).json({status: httpStatusText.ERROR, Message: error.Message});
     }
 }
 // ============================
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -6,19 +7,17 @@ const coursesRouter = require('./routers/courses.routers')
 app.use(express.json())
 app.use('/api/courses',coursesRouter)
 
-const mongoAtlasUri =
-"mongodb+srv://zainab:1234@mycluster.feybrsm.mongodb.net/codeZone_courses?retryWrites=true&w=majority&appName=myCluster";
+
 const connectDB = async () => {
     try {
       // Connect to the MongoDB cluster
-      await mongoose.connect(mongoAtlasUri);
+      await mongoose.connect(process.env.mongoAtlasUri);
       console.log(" ===Mongoose is connected");
     } catch (e) {
       console.log("could not connect");
     }
 }
 connectDB();
-
 app.listen(5000,()=>{
     console.log('listing on port 5000');
 })
